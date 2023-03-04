2d237d2c9f5e27133c5958df335782e1f32d9c9a
murtada666 <bvvgg81@gmail.com>
2023-03-04 14:04:38 +0000

    update on the endpoints

--- travel/api/agency.py
+++ travel/api/agency.py
@@ -1,10 +1,8 @@
 from typing import List
 from ninja import Router
 
-from rest_framework.exceptions import NotFound          
-
 from travel.models import Agency, Trip
-from travel.schemas import AgencyOut, TripsOut
+from travel.schemas import AgencyOut, TripsOut, MessageOut
 from django.shortcuts import get_object_or_404
 
 
@@ -13,24 +11,24 @@ from django.shortcuts import get_object_or_404
 agency_router = Router(tags=["agencies"])
 
 
-@agency_router.get("/all_agencies", response=List[AgencyOut])
-def all_(request):
+@agency_router.get("/all_agencies", response={200:List[AgencyOut], 404:MessageOut, 500:MessageOut, 400:MessageOut})
+def all(request):
     agencies = Agency.objects.all()
-    
-    if not agencies:
-        return "NO agency was FOUND"
+
+    if len(agencies) == 0:
+        return 404, {'message': "NO agency was FOUND"}
     return agencies
-    
+
 
 @agency_router.get("/agency_trips", response=List[TripsOut])
 def all_trips(request, id: int):
-    
+
     agency = get_object_or_404(Agency, id=id)
-    
+
     trips = agency.trips.all()
-    
-    if not trips:
-        return "there are NO trips"
+
+    if len(trips) == 0:
+        return 404, {'message':"NO trips were FOUND"}
     return trips
-    
-  
+
+
